6a517c99f8779247eca02dcf259ac0c9780c1bc3
Zhao Changlin <huskydawg@foxmail.com>
2023-07-17 15:04:08 +0800

    Add several constants

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,29 +1,33 @@
 """
 CP1404/CP5632 - Practical
-Broken program to determine score status
+Use score to determine result
 """
 from random import *
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+EXCELLENT_THRESHOLD = 90
+PASSABLE_THRESHOLD = 50
 
 
 def main():
-    """Get score and print status."""
+    """Get score and print result."""
     score = float(input("Enter score: "))
     result = determine_result(score)
 
     print(result)
 
-    random_score = randint(0, 100)
+    random_score = randint(MINIMUM_SCORE, MAXIMUM_SCORE)
     random_result = determine_result(random_score)
     print(f"A random score of {random_score} is {random_result}")
 
 
 def determine_result(score):
     """Determine result based on score."""
-    if score < 0 or score > 100:
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
         result = "Invalid score"
-    elif score >= 90:
+    elif score >= EXCELLENT_THRESHOLD:
         result = "Excellent"
-    elif score >= 50:
+    elif score >= PASSABLE_THRESHOLD:
         result = "Passable"
     else:
         result = "Bad"
