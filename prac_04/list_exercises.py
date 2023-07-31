43a463e9928c6bc7499782507eedaeb0bbad4686
Zhao Changlin <huskydawg@foxmail.com>
2023-07-31 13:17:46 +0800

    Improve the program using sum()

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -6,17 +6,15 @@ NUMBER_OF_PROMPTS = 5
 def main():
     """Get numbers and display info."""
     numbers = []
-    total = 0
     for i in range(NUMBER_OF_PROMPTS):
         number = int(input("Number: "))
         numbers.append(number)
-        total += number
 
     print(f"The first number is {numbers[0]}")
     print(f"The last number is {numbers[-1]}")
     print(f"The smallest number is {min(numbers)}")
     print(f"The largest number is {max(numbers)}")
-    print(f"The average of the numbers is {total / NUMBER_OF_PROMPTS:.1f}")
+    print(f"The average of the numbers is {sum(numbers) / NUMBER_OF_PROMPTS:.1f}")
 
     # Woefully inadequate security checker
     usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
