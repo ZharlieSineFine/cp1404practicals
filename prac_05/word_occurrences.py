6bd927ee4b6440edeecca1a77983d688929e2de8
Zhao Changlin <huskydawg@foxmail.com>
2023-08-07 14:19:02 +0800

    Complete the program as requested

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,7 +1,15 @@
 """
 Word Occurrences
-Estimate: 20 minutes
-Actual:   32 minutes
+Estimate: 10 minutes
+Actual:   16 minutes
 """
 
 user_text = input("Text: ")
+word_to_count = {}
+for word in user_text.split():
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+
+maximum_length = max(len(word) for word in list(word_to_count.keys()))
+
+for word, count in sorted(word_to_count.items()):
+    print(f"{word:<{maximum_length}} : {count}")
