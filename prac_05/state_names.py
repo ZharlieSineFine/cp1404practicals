a16387ea83cf1da3ecb9c7dbe80219e419382718
Zhao Changlin <huskydawg@foxmail.com>
2023-08-07 12:46:37 +0800

    Improve the program as requested

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,14 +5,23 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()  # Change the input to uppercase
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    # if state_code in CODE_TO_NAME:
+    #     print(state_code, "is", CODE_TO_NAME[state_code])
+    # else:
+    #     print("Invalid short state")
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+
+# Write a loop that prints all the states and names neatly lined up with string formatting
+maximum_length = max(len(name) for name in list(CODE_TO_NAME.keys()))
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:<3} is {name:<{maximum_length}}")
