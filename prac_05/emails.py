b041d20cb139a91ef8526099de721f6040a79427
Zhao Changlin <huskydawg@foxmail.com>
2023-08-07 15:08:49 +0800

    Implement a function extract_name_by_email()

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,20 +3,30 @@ Emails
 Estimate: 15 minutes
 Actual:   25 minutes
 """
-user_email = input("Email: ")
-email_to_name = {}
-while user_email != "":
-    name_components = user_email.split("@")[0].split(".")
-    user_name = " ".join(name_components).title()
-    name_answer = input(f"Is your name {user_name.title()}? (Y/n) ").upper()
-    if name_answer != "" and name_answer != "Y":
-        user_name = input("Name: ")
 
-    email_to_name[user_email] = user_name
 
+
+
+def main():
     user_email = input("Email: ")
+    email_to_name = {}
+    while user_email != "":
+        user_name = extract_name_by_email(user_email)
+        name_answer = input(f"Is your name {user_name.title()}? (Y/n) ").upper()
+        if name_answer != "" and name_answer != "Y":
+            user_name = input("Name: ")
+
+        email_to_name[user_email] = user_name
+
+        user_email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 
-for email, name in email_to_name.items():
-    print(f"{name} ({email})")
+def extract_name_by_email(email):
+    name_components = email.split("@")[0].split(".")
+    user_name = " ".join(name_components).title()
+    return user_name
+
 
