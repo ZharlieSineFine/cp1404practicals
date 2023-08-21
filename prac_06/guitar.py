eb2d74394c50c62bd9f89dc66bcc17f7a650b2ee
Zhao Changlin <huskydawg@foxmail.com>
2023-08-21 11:28:51 +0800

    Implement the constructor

---
+++ prac_06/guitar.py
@@ -0,0 +1,13 @@
+"""CP1404/CP5632 Practical - Guitar class."""
+
+
+class Guitar:
+    """Represent a guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Construct a guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+
