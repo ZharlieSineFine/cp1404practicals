813b6638c36a8453da64cb02d08a9c6234154a6b
Zhao Changlin <huskydawg@foxmail.com>
2023-08-21 10:55:51 +0800

    Implement a function named is_dynamic

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -8,10 +8,15 @@ class ProgrammingLanguage:
     """Represent a programming language object."""
 
     def __init__(self, name="", typing="", reflection=True, year=None):
+        """Initialize a programming language instance."""
         self.name = name
         self.typing = typing.title()
         self.reflection = reflection
         self.year = year
 
-
-
+    def is_dynamic(self):
+        """Check if the language typing is dynamic."""
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
