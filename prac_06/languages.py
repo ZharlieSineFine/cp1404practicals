72b0e7e13233d567f11836b55c195e078e34c989
Zhao Changlin <huskydawg@foxmail.com>
2023-08-21 11:11:13 +0800

    Produce the requested output

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -4,10 +4,17 @@ from cp1404practicals.prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
+    """Test programming language class."""
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
     print(python)
+
+    programming_languages = [python, ruby, visual_basic]
+    dynamic_typing_languages = [language for language in programming_languages if language.is_dynamic()]
+    print("The dynamically typed languages are:")
+    for dynamic_language in dynamic_typing_languages:
+        print(dynamic_language.name)
 
 
 if __name__ == '__main__':
