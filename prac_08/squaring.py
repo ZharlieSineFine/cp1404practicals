9db409d2d1c7e715c3471d3c173317661500b983
Zhao Changlin <huskydawg@foxmail.com>
2023-09-04 10:07:18 +0800

    Change the docstring

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -1,15 +1,15 @@
 """
 CP1404/CP5632 Practical
 Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
-Started 13/10/2015
+Zhao Changlin, IT@JCU
+Started 04/08/2023
 """
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
+__author__ = 'Zhao Changlin'
 
 
 class SquareNumberApp(App):
