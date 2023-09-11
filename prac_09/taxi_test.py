4e771dc458c232b1a409c3ec35ee26e10ac54538
Zhao Changlin <huskydawg@foxmail.com>
2023-09-11 15:17:02 +0800

    Improve the test code

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,7 +7,9 @@ from taxi import Taxi
 
 
 def main():
+    """Test the Taxi class with its method."""
     new_taxi = Taxi(name="Prius 1", fuel=100)
+    print(new_taxi)
     new_taxi.start_fare()
     new_taxi.drive(40)
     print(f"{new_taxi}, and the fare is ${new_taxi.get_fare()}")
