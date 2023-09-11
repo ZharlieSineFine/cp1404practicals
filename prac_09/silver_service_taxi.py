85301a580f1940fdbd4e59840d083dbb1f0a1cc6
Zhao Changlin <huskydawg@foxmail.com>
2023-09-11 11:01:51 +0800

    Implement specified get_fare method for SilverServiceTaxi

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -5,6 +5,7 @@ SilverServiceTaxi class
 
 from taxi import Taxi
 
+
 class SilverServiceTaxi(Taxi):
     """A specific variation of Taxi class."""
 
@@ -20,4 +21,6 @@ class SilverServiceTaxi(Taxi):
         """Return a string representation of a SilverServiceTaxi."""
         return f"{super().__str__()}, the fanciness is {self.fanciness}, plus flagfall is  {self.flagfall:.2f}."
 
-
+    def get_fare(self):
+        """Return the silver service taxi fare."""
+        return super().get_fare() + self.flagfall
